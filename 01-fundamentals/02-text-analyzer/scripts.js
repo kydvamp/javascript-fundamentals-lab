3c9714761970a05433a0e1e8d603ab0103adef6b
codedbyky <kyyyagullano@gmail.com>
2026-01-08 02:36:15 +0800

    Add initial element references for Text Analyzer functionality

---
+++ 01-fundamentals/02-text-analyzer/scripts.js
@@ -0,0 +1,10 @@
+let inputTextEl = document.getElementById("input-text" );
+let wordCountEl = document.getElementById("wordCount");
+let charCountEl = document.getElementById("charCount");
+let sentenceCountEl = document.getElementById("sentenceCount");
+
+
+
+
+
+
